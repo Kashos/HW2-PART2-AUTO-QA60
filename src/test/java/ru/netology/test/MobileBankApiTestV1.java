0e3a61735e804a64a4aa7ec1ecbb06487d5632ae
Egor Kachesov <kashos152@gmail.com>
2023-06-23 18:42:19 +0700

    Tests fail

--- src/test/java/ru/netology/test/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/test/MobileBankApiTestV1.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("scary, scary"))
+                .body("data", equalTo("very scary"))
         ;
     }
 }
