6522c6303695773bd969efcbed5af4f89c478e4c
Egor Kachesov <kashos152@gmail.com>
2023-06-23 18:33:15 +0700

    Initial commit

---
+++ src/test/java/ru/netology/test/MobileBankApiTestV1.java
@@ -0,0 +1,26 @@
+package ru.netology.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldPostSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("scary, very scary") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("scary, very scary"))
+        ;
+    }
+}
